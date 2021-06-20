44217dc865c11ae7f843a1b6805614e8623b2f45
Marc8s <marcus.ecomp@gmail.com>
2021-06-20 17:26:35 +0100

    Presentation slides done

--- app/src/main/java/com/example/pasargada/MainActivity.java
+++ app/src/main/java/com/example/pasargada/MainActivity.java
@@ -4,11 +4,38 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
+
+public class MainActivity extends IntroActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        //remove butões back e next dos slides
+        setButtonBackVisible(false);
+        setButtonNextVisible(false);
+
+        addSlide(new FragmentSlide.Builder()
+                .background(R.color.purple_200)
+                .fragment(R.layout.intro_1)
+                .build()
+        );
+        addSlide(new FragmentSlide.Builder()
+                .background(R.color.purple_200)
+                .fragment(R.layout.intro_2)
+                .build()
+        );
+        addSlide(new FragmentSlide.Builder()
+                .background(R.color.purple_200)
+                .fragment(R.layout.intro_3)
+                .build()
+        );
+        addSlide(new FragmentSlide.Builder()
+                .background(R.color.purple_200)
+                .fragment(R.layout.intro_4)
+                .build()
+        );
     }
 }
