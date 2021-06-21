02e9008429f6d61965cdc93ba822c5e780209c19
Marc8s <marcus.ecomp@gmail.com>
2021-06-21 15:09:01 +0100

    Firebase configuration

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ The main goal of this app is to help immigrants to understand the burocracy behi
 There´is a poem write by the brazilian poet Manuel Bandeira called "Vou-me embora pra Pasárgada". The poem talk about the desire of move on to Pasargada,
 one special land where life can be better.
 # Progress
-1. Presentation slides using a material-intro library from https://github.com/heinrichreimer/material-intro (Done)
-2. Firebase Configuration (doing)
+1. Presentation slides using a material-intro library from https://github.com/heinrichreimer/material-intro (done)
+2. Firebase Configuration (done)
 3. List of available countries (to do)
 
--- app/src/main/java/com/example/pasargada/MainActivity.java
+++ app/src/main/java/com/example/pasargada/MainActivity.java
@@ -4,19 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
 import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
 
 public class MainActivity extends IntroActivity {
+    //recupera a instancia e aponta para o nó raiz do firebase
+    private DatabaseReference mReferenceDatabase = FirebaseDatabase.getInstance().getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        //DESCOMENTAR PARA ADICIONAR PAIS NO FIREBASE
+        //addCountryFirebase();
         //remove botões back e next dos slides
         setButtonBackVisible(false);
         setButtonNextVisible(false);
-
+        //slides apresentados ao abrir a app
         addSlide(new FragmentSlide.Builder()
                 .background(R.color.background_sliders)
                 .fragment(R.layout.intro_1)
@@ -32,5 +37,13 @@ public class MainActivity extends IntroActivity {
                 .fragment(R.layout.intro_3)
                 .build()
         );
+    }
+    //Chamar esse método quando quiser adicionar um pais no firebase
+    //Se parecer tudo correto e não estiver funcionando o realtimedatabase experimentar Build>Clean Projet e depois Build>Rebuild Projet
+    public void addCountryFirebase(){
+        DatabaseReference countrys = mReferenceDatabase.child("countrys");
+        Country country = new Country();
+        country.setName("Portugal");
+        countrys.push().setValue(country);
     }
 }
