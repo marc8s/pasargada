081c76403faf9f4cddd138ef4d0591da6e6405a9
Marc8s <marcus.ecomp@gmail.com>
2021-07-01 10:29:27 +0100

    changes at class documents

--- app/src/main/java/com/example/pasargada/model/Document.java
+++ app/src/main/java/com/example/pasargada/model/Document.java
@@ -1,9 +1,20 @@
 package com.example.pasargada.model;
 
+import com.example.pasargada.config.ConfigFirebase;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Exclude;
+
 public class Document {
     private String name, id, description, steps;
 
     public Document() {
+    }
+
+    public void save(){
+        DatabaseReference databaseReference = ConfigFirebase.getFirebaseDatabase();
+        databaseReference.child("documents")
+                .child(this.id)
+                .setValue(this);
     }
 
     public String getName() {
@@ -14,6 +25,7 @@ public class Document {
         this.name = name;
     }
 
+    @Exclude
     public String getId() {
         return id;
     }
